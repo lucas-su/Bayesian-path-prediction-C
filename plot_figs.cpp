c21cdc5ac417fea61faf9fc5db6c96b77d10ae52
lucas-su <lucas.schootuiterkamp@gmail.com>
2023-06-13 12:13:01 +0200

    update project

---
+++ plot_figs.cpp
@@ -0,0 +1,22 @@
+#include "BPP.h"
+#include "Model.h"
+#include <cmath>
+#include <matplot/matplot.h>
+// #include "gnuplot-iostream.h"
+
+int plot(Model* model){
+    // Gnuplot gp("script.gp");
+    // gp << "plot '-' with vectors title 'pts_A', '-' with vectors title 'pts_B'\n";
+    // using namespace matplot;
+    // auto t = iota(0, pi / 20, 10 * pi);
+    // auto xt = transform(t, [](auto t) { return sin(t); });
+    // auto yt = transform(t, [](auto t) { return cos(t); });
+    // auto l = plot3(xt, yt, t, "-ob");
+    // l->marker_size(10);
+    // l->marker_face_color("#D9FFFF");
+    // xlabel("x(t)");
+    // ylabel("y(t)");
+    // zlabel("t");
+    // show();
+    return 0;
+}
